170c1bbfb2428773e4f4f0b08c0ab6d4b6493050
admin <admin@hp>
2018-03-24 22:43:42 +0530

    POST API generated

--- RestApplication_2/src/main/java/com/naveen/src/MessageResources.java
+++ RestApplication_2/src/main/java/com/naveen/src/MessageResources.java
@@ -2,7 +2,9 @@ package com.naveen.src;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,17 +18,26 @@ public class MessageResources {
 	MessageService messageser=new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
  public List<Message> getMessages(){
 	 return messageser.getAllMessages();
 	 //returning a List of messages 
  }
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/test/{id}")
 	public Message getMessage(@PathParam("id")  Long id){
 		return messageser.getMessage(id);
 	}
+	@POST
+	/*@Path("/addmessage")*/
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message){
+		return messageser.addMessage(message);
+	}
+
+	
 	
 	
 	
