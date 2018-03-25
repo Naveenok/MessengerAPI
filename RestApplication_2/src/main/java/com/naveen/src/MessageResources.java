141c40cb0ed272b900d135fdade17629a4dfb2c5
admin <admin@hp>
2018-03-25 11:52:13 +0530

    PUT and DELETE method updated

--- RestApplication_2/src/main/java/com/naveen/src/MessageResources.java
+++ RestApplication_2/src/main/java/com/naveen/src/MessageResources.java
@@ -3,8 +3,10 @@ package com.naveen.src;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,6 +16,10 @@ import com.naveen.model.Message;
 import com.naveen.service.MessageService;
 
 @Path("/messages")
+//if all the methods uses produces or consumes then we can put a common produces & consumes at class level
+//like below one
+/*@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)*/
 public class MessageResources {
 	MessageService messageser=new MessageService();
 	
@@ -30,17 +36,27 @@ public class MessageResources {
 		return messageser.getMessage(id);
 	}
 	@POST
-	/*@Path("/addmessage")*/
+	/*@Path("/addmessage")*/    
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message){
 		return messageser.addMessage(message);
 	}
-
-	
-	
-	
-	
+	@PUT
+	@Path("/test/{messageid}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageid")Long id,Message message){
+		message.setId(id);
+		return messageser.updateMessage(message);
+	}
+	 
+	@DELETE 
+	@Path("/test/{mesid}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("mesid") Long id){
+		 messageser.removeMessage(id);
+	}
 	
 }
    
